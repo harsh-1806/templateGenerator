ac185bb72dfa1aeb1ea8d20bec574203a403203d
= <helloharsh51@gmail.com>
2023-07-11 15:06:43 +0530

    Changed some button features

--- js/main.js
+++ js/main.js
@@ -2,18 +2,29 @@ const btn = document.getElementById('vscode-button');
 
 const div = document.getElementById('vscode-page');
 
+const subBtn = document.getElementById('sublime-button');
+
+const output = document.getElementById('out');
+
+const vsCodeOut =  '"": {<br>"prefix": "",<br>"body": [<br>""<br> ], <br>"description": ""<br>}';
+
+const copyBtn = document.getElementById('copy-button');
+
+
+// Todo: add snippet for Sublime Text.
+
 btn.addEventListener('click', () => {
+    output.innerHTML = vsCodeOut;
+    subBtn.style.backgroundColor = '#00000040';
     div.style.backgroundColor = '#006eb4';
     btn.style.backgroundColor = '#006eb4';
 })
 
 
-const text = document.getElementById('snippetInput');
 
-const output = document.getElementById('out');
-
-// text.addEventListener('input', () => {
-//     output.innerText = (text.value) => {
-
-//     };
-// });
+subBtn.addEventListener('click', () => {
+    // output.innerHTML = sublimeOut;
+    btn.style.backgroundColor = '#00000040';
+    div.style.backgroundColor = '#2b303b';
+    subBtn.style.backgroundColor = '#2b303b';
+});
